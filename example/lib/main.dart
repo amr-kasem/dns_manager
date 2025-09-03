618e4cdd1c2b3440a93f30fb40e2bfe477de3372
Amr Kasem <amr.kasem.b57@gmail.com>
2025-09-03 13:33:40 +0300

    latest

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:dns_manager/dns_manager.dart';
 import 'package:dns_manager/dns_manager_method_channel.dart';
 
